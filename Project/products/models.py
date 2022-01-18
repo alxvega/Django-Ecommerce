6ee456bd7f25df4e4dc2c207436d1dbb643d61af
Alex Vega <alexsantiagovega99@gmail.com>
2022-01-18 10:15:04 -0300

    got the unique slugs

--- Project/products/models.py
+++ Project/products/models.py
@@ -1,4 +1,6 @@
+import uuid
 from enum import unique
+
 from django.db import models
 from django.utils.text import slugify
 from django.db.models.signals import pre_save
@@ -20,6 +22,12 @@ class Product(models.Model):
 
 
 def set_slug(sender, instance, *args, **kwargs):
-    instance.slug = slugify(instance.title)
+    if instance.title and not instance.slug:
+        slug = slugify(instance.title)
+        while Product.objects.filter(slug=slug).exists():
+            slug = slugify(
+                '{}-{}'.format(instance.title, str(uuid.uuid4()))[:8])
+        instance.slug = slug
+
 
 pre_save.connect(set_slug, sender=Product)
