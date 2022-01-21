56a244a71e0995b61e8c570870a330f5ac2c4fa0
Alex Vega <alexsantiagovega99@gmail.com>
2022-01-21 18:05:49 -0300

    checking cart session and auth

--- Project/carts/views.py
+++ Project/carts/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
+from .models import Cart
 
 
 def cart(request):
+    user = request.user if request.user.is_authenticated else None
+    cart_id = request.session.get('cart_id')
+
+    if cart_id:
+        cart = Cart.objects.get(pk=request.session.get('cart_id'))
+    else:
+        cart = Cart.objects.create(user=user)
+    request.session['cart_id'] = cart.id
     return render(request, 'carts/cart.html', {})
 
 # Create your views here.
