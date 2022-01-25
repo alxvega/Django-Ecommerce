c57cb3708fcddaa9e8f7fdf3031b675f17351362
Alex Vega <alexsantiagovega99@gmail.com>
2022-01-25 19:46:07 -0300

    products to cart working

--- Project/carts/views.py
+++ Project/carts/views.py
@@ -8,7 +8,7 @@ from .utils import get_or_create_cart
 
 def cart(request):
     cart = get_or_create_cart(request)
-    return render(request, 'carts/cart.html', {})
+    return render(request, 'carts/cart.html', {'cart':cart})
 
 
 def add(request):
