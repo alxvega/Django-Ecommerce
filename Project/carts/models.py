356eefda9b4ebcb1b2ab41f7486cb6773c5a8dcf
Alex Vega <alexsantiagovega99@gmail.com>
2022-01-25 20:46:40 -0300

    working with cart callback functions

--- Project/carts/models.py
+++ Project/carts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from users.models import User
 from products.models import Product
 from django.db.models.signals import pre_save
+from django.db.models.signals import m2m_changed
 import uuid
 
 
@@ -15,14 +16,28 @@ class Cart(models.Model):
     subtotal = models.DecimalField(default=0.0, max_digits=8, decimal_places=2)
     total = models.DecimalField(default=0.0, max_digits=8, decimal_places=2)
     created = models.DateField(auto_now_add=True)
+    FEE = 0.5
 
     def __str__(self):
         return (f'{self.cart_id}')
+
+    def update_totals(self):
+        self.update_subtotal()
+        self.update_total = self.subtotal + (self.subtotal * decimal.Decimal(self.Fee))
+
+    def update_subtotal(self):
+        self.subtotal = sum([product.price for product in self.products.all()])
+        self.save()
 
 
 def set_cart_id(sender, instance, *args, **kwargs):
     if not instance.cart_id:
         instance.cart_id = str(uuid.uuid4())
+
+
+def update_totals(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        instance.update_totals()
 
 
 pre_save.connect(set_cart_id, sender=Cart)
--- Project/carts/views.py
+++ Project/carts/views.py
@@ -1,14 +1,16 @@
 from pickle import NONE
 from webbrowser import get
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Cart
+from django.shortcuts import redirect
 from products.models import Product
 from .utils import get_or_create_cart
+from django.shortcuts import get_list_or_404
 
 
 def cart(request):
     cart = get_or_create_cart(request)
-    return render(request, 'carts/cart.html', {'cart':cart})
+    return render(request, 'carts/cart.html', {'cart': cart})
 
 
 def add(request):
@@ -16,4 +18,11 @@ def add(request):
     product = Product.objects.get(pk=request.POST.get('product_id'))
     cart.products.add(product)
 
-    return render(request, 'carts/add.html', {'product':product})
+    return render(request, 'carts/add.html', {'product': product})
+
+
+def remove(request):
+    cart = get_or_create_cart(request)
+    product = get_object_or_404(Product, pk=request.POST.get('product_id'))
+    cart.products.remove(product)
+    return redirect('carts:cart')
