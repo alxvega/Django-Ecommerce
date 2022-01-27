228535bdb8975007b300485711f2780689a86cf5
Alex Vega <alexsantiagovega99@gmail.com>
2022-01-27 16:42:29 -0300

    fixed the filters

--- Project/carts/views.py
+++ Project/carts/views.py
@@ -24,7 +24,7 @@ def add(request):
                                                                   product=product,
                                                                   quantity=quantity)
 
-    return render(request, 'carts/add.html', {'product': product})
+    return render(request, 'carts/add.html', {'quantity': quantity, 'cart_product': cart_product, 'product': product})
 
 
 def remove(request):
